befe67acacf53586d47434371c7b9ea154e1cc43
SharkyTheWhite <91147507+SharkyTheWhite@users.noreply.github.com>
2022-08-24 18:41:49 +0200

    Add QR Code for easy access to deployed version.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ A tool to easily create facial expressions for your pixel-based visor.
 
 
 ## Live Preview Build 
+<img src="src/assets/ghpages-qrcode.png" align="right" width="200" height="200" alt="QR Code to launch ProtoFace Designer">
+
 [![Deploy to GitHub Pages](https://github.com/SharkyTheWhite/protoface-designer/actions/workflows/deploy-gh-pages.yml/badge.svg)](https://github.com/SharkyTheWhite/protoface-designer/actions/workflows/deploy-gh-pages.yml)
 
 You can see and try the current state of the tool in your browser here:
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,27 @@
   <div class="about">
     <h1>ProtoFace Designer</h1>
     <p>
-      A tool to easily create facial expressions for your pixel-based visor.
+      A tool to easily create facial expressions for your pixel-based visor.<br>
+      <a href="https://github.com/SharkyTheWhite/protoface-designer">Show project on GitHub</a>
     </p>
-    <p class="warning">Draft Project - no functionality yet.</p>
+    <p class="warning">Draft Project - limited functionality - use at your own risk</p>
+    <p>
+      Use this QR Code to start the ProtoFace Designer from github pages:<br>
+      <img alt="QR Code to start ProtoFace Designer" :src="require('@/assets/ghpages-qrcode.png')" class="qrcode"><br>
+      QR-Code Links to: <a href="https://sharkythewhite.github.io/protoface-designer/">sharkythewhite.github.io/protoface-designer</a>
+    </p>
   </div>
 </template>
 
 <style scoped>
 .warning {
   color: crimson; font-weight: bold; font-style: italic
+}
+.qrcode {
+  width: 80vw;
+  height: 80vh;
+  max-height: 20em;
+  max-width: 20em;
+  object-fit: contain;
 }
 </style>
